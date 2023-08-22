cba3c69ff9983b17f21134df4d984a982dc62c52
SebasRussi <98978409+SebasRussi@users.noreply.github.com>
2023-08-22 13:02:51 -0500

    Update todo.py

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,8 +1,35 @@
 class Todo:
-    pass
+    def __init__(self,code_id:int, tittle: str, description: str):
+        self.code_id: int = code_id
+        self.tittle: str = tittle
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.tittle}"
 
 
 class TodoBook:
-    pass
+    def __init__(self):
+        self.todos: dict[int, Todo] = {}
 
+    def add_todo(self,tittle: str, descriptiom: str) -> int:
+        code_id: int = len(self.todos) + 1
+        todo: Todo = Todo(code_id, tittle, descriptiom)
+        self.todos[code_id] = todo
+        return  code_id
+
+    def pending_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if not todo.completed]
+
+    def completed_todos(self) -> list[Todo]:
+        return [todo for todo in self.todos.values() if todo.completed]
 
